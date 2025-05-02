46333fa90a39a0bbe6f8ffc2fe8914120feb1d6d
vl4ks <veli4koksenia@yandex.ru>
2025-05-02 13:32:58 +0500

    обработан случай повторного добавления устройства в хаб

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/handlers/DeviceAdded.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/handlers/DeviceAdded.java
@@ -17,9 +17,32 @@ public class DeviceAdded implements HubEventHandler {
 
     @Override
     @Transactional
+//    public void handle(HubEventAvro hubEvent) {
+//        Sensor savedSensor = sensorRepository.save(buildToSensor(hubEvent));
+//        log.info("Датчик успешно сохранен в БД: id={}", savedSensor.getId());
+//    }
     public void handle(HubEventAvro hubEvent) {
-        Sensor savedSensor = sensorRepository.save(buildToSensor(hubEvent));
-        log.info("Датчик успешно сохранен в БД: id={}", savedSensor.getId());
+        DeviceAddedEventAvro deviceAddedEvent = (DeviceAddedEventAvro) hubEvent.getPayload();
+        String deviceId = deviceAddedEvent.getId();
+        String hubId = hubEvent.getHubId();
+
+        sensorRepository.findById(deviceId)
+                .ifPresentOrElse(
+                        existingSensor -> {
+                            if (!existingSensor.getHubId().equals(hubId)) {
+                                existingSensor.setHubId(hubId);
+                                sensorRepository.save(existingSensor);
+                                log.info("Обновлен хаб для существующего датчика: id={}, новый hubId={}",
+                                        deviceId, hubId);
+                            } else {
+                                log.debug("Датчик уже существует и привязан к этому хабу: id={}", deviceId);
+                            }
+                        },
+                        () -> {
+                            Sensor newSensor = sensorRepository.save(buildToSensor(hubEvent));
+                            log.info("Датчик успешно сохранен в БД: id={}", newSensor.getId());
+                        }
+                );
     }
 
     @Override

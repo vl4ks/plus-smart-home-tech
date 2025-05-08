e41848929cfa8455203898d37be31f7ff62fcbf3
vl4ks <veli4koksenia@yandex.ru>
2025-05-09 01:21:25 +0500

    Выполнено задание по теме настройка сервера конфигураций

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApplication.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CollectorApplication {
 
     public static void main(String[] args) {

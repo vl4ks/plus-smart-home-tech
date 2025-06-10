d9a3d82b5eae51ea0ca6d694032605be94b811af
vl4ks <veli4koksenia@yandex.ru>
2025-06-10 23:38:25 +0500

    add OrderApplication

---
+++ commerce/order/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS orders
+(
+    order_id         UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    shopping_cart_id UUID NOT NULL,
+    payment_id       UUID,
+    delivery_id      UUID,
+    state            VARCHAR(50),
+    delivery_weight  DOUBLE PRECISION,
+    delivery_volume  DOUBLE PRECISION,
+    fragile          BOOLEAN,
+    total_price      DOUBLE PRECISION,
+    delivery_price   DOUBLE PRECISION,
+    product_price    DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS order_items
+(
+    order_id   UUID REFERENCES orders (order_id) ON DELETE CASCADE,
+    product_id UUID NOT NULL,
+    quantity   INTEGER
+);
